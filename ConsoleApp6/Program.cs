b86301cb7ac8689d50d83be09e9874c201471570
unknown <perviz.elizade.2002@mail.ru>
2022-11-13 16:18:12 +0400

    first commit

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1. Ədədin 2-nin qüvvəti olub olmadığı tapan alqoritm.
+            //Console.WriteLine("Eded daxil edin:");
+            //int eded = Convert.ToInt32(Console.ReadLine());
+
+            //while (eded % 2 == 0)
+            //{
+            //    eded /= 2;
+            //    if (eded == 1)
+            //    {
+            //        Console.WriteLine("eded 2in ustudu");
+            //        return;
+            //    }
+            //}
+            //Console.WriteLine("2in quvveti deyil");
+            #endregion
+
+
+            #region 2. Verilen 2 eded arasindaki ededleri ekrana yazdiran alqoritm. 
+            //Console.WriteLine("Ilk ededi daxil edin:");
+            //int ilkeded = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Ikinci ededi daxil edin:");
+            //int ikincieded = Convert.ToInt32(Console.ReadLine());
+            //for (int i = ilkeded+1; i < ikincieded; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
+
+
+            #region 3. Verilen eded 70-den boyukdurse, 70-le arasindaki ferqin 3 qatini ekrana yazdiran, eger kicikdirse ferqin ozunu ekrana yazdiran alqoritm.
+
+            //Console.WriteLine("Eded daxil edin");
+            //int eded = Convert.ToInt32(Console.ReadLine());
+            //if (eded>70)
+            //{
+            //    Console.WriteLine((eded-70)*3);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(70-eded);
+            //}
+            #endregion
+
+            #region 4. X, Y və Z ədədləri verilir. Hansı ədədin böyük olduğunu ekrana çıxardan bir alqoritm yazırsız.
+
+            //Console.WriteLine("3 eded daxil edin:");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //int b = Convert.ToInt32(Console.ReadLine());
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //if (a>b && a>c)
+            //{
+            //    Console.WriteLine("A en boyukdur");
+            //}
+            //else if (b>a && b>c)
+            //{
+            //    Console.WriteLine("B en boyukdur");
+            //}
+
+            //else if (c>a && c>b)
+            //{
+            //    Console.WriteLine("C en boyukdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Ededleri ferqli daxil edin:");
+            //}
+
+            #endregion
+
+            #region 5. Verilmiş n və m (n<m) ədədləri arasında neçə tək ədəd olduğunu tapın.
+            Console.WriteLine("Ilk ededi daxil edin:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ikinci ededi daxil edin:");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = n+1; i < m; i++)
+            {
+                if (i%2!=0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            #endregion
+        }
+    }
+}
